97ca5503762a88f126320909e084dc304a2afb57
Pawan Kumar <pkprasad06@gmail.com>
2023-03-16 16:15:08 +0530

    refactored

--- src/UI/Button.js
+++ src/UI/Button.js
@@ -1,13 +1,8 @@
 import classes from './UI.module.css'
 
-
-const Button = (props) => {
-    // const { variant, onClick, children} = props;
-    const { variant, children, ...rest} = props;
-    return (
-        <button {...rest} className={`${classes.button} ${classes[variant]}`} >
-            {children}
-        </button>
-    )
-}
+const Button = ({ variant, children, ...rest}) => (
+    <button {...rest} className={`${classes.button} ${classes[variant]}`} >
+        {children}
+    </button>
+)
 export default Button;
